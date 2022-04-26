50cb0f8fc044c8e68ae5c6fa6dcca6c5a0623eea
Antonio <don_ne@mail.ru>
2022-04-26 16:13:58 +0400

    Rep

---
+++ les03/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 32:** Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+using System;
+
+Console.Clear();
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(" ",array));
+int a = 0;
+while(a<12)
+{
+    Console.Write($"{array[a]*-1} ");
+    a=a+1;
+}
+
+
+
+
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++ )
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
---
+++ les04/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 33:** Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//4; массив [6, 7, 19, 345, 3] -> нет
+//3; массив [6, 7, 19, 345, 3] -> да
+
+using System;
+
+Console.Clear();
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(" ",array));
+
+int a = 0;
+int num = 4;
+
+while(a<12)
+{
+    if (array[a]==num)
+    {
+        Console.WriteLine($"{num}; -> Есть ");
+        return;
+    }
+    else
+    {
+      a++;
+    }
+}
+Console.WriteLine($"{num}; -> нет ");
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++ )
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
